9516a190cf7adf96e490f02dc7e2c8ae275eb44d
Ares-Santos <alex.ares.santos@gmail.com>
2024-09-20 12:13:46 +0100

    brain die

--- LAB1/LAB1.srcs/sim_1/new/circuit_tb.vhd
+++ LAB1/LAB1.srcs/sim_1/new/circuit_tb.vhd
@@ -47,7 +47,7 @@ architecture Behavioral of circuit_tb is
     signal clk  :std_logic := '0';
     signal reset  :std_logic := '0';
     signal  ADD_SUB, LOAD, MULS, LOGIC_SHIFT, switch     :  std_logic := '0';
-	signal data 	: std_logic_vector(15 downto 0);
+	signal data 	: std_logic_vector(9 downto 0);
 
      -- Clock period definitions
     constant clk_period : time := 10 ns;
@@ -79,24 +79,41 @@ begin
         reset <= '1' after 20 ns,
                  '0' after 40 ns;
         
-        data <= X"04" after 40 ns,
-                X"08" after 80 ns,
-                X"0E" after 120 ns;
+        data <= "00" & X"04" after 40 ns,
+                "00" & X"08" after 80 ns,
+                "00" & X"0E" after 140 ns,
+                "00" & X"02" after 320 ns;
         
         
         LOAD <= '1' after 40 ns,
-                '0' after 50 ns,
+                '0' after 60 ns,
                 '1' after 80 ns,
-                '0' after 90 ns;
+                '0' after 100 ns,
+                '1' after 140 ns,
+                '0' after 160 ns,
+                '0' after 320 ns;
                 
                  
         switch <= '1' after 80 ns,
-                  '0' after 90 ns;
-                    
+                  '0' after 140 ns,
+                  '1' after 200 ns,
+                  '0' after 280 ns;  
         
-        ADD_SUB <= '1' after 100 ns;
+        ADD_SUB <= '1' after 120 ns,
+                   '0' after 140 ns,
+                   '1' after 160 ns,
+                   '0' after 180 ns,
+                   '1' after 300 ns,
+                   '0' after 320 ns;
+                   
+        LOGIC_SHIFT <= '1' after 180 ns,
+                       '0' after 200 ns,
+                       '1' after 220 ns,
+                       '0' after 240 ns;  
         
-        
+        MULS <= '1' after 260 ns,
+                '0' after 280 ns;
+        wait;
         
     end process;
     
--- LAB1/LAB1.srcs/sources_1/new/datapath.vhd
+++ LAB1/LAB1.srcs/sources_1/new/datapath.vhd
@@ -17,8 +17,9 @@ architecture behavioral of datapath is
 	signal mux_a, logic    			: std_logic_vector (15 downto 0);
 	signal register1 				: std_logic_vector (9 downto 0);
 	signal data_1, data_2 : signed(15 downto 0);
-    signal add_sub , mul : signed (15 downto 0);
+    signal add_sub  : signed (15 downto 0);
     signal shift : unsigned(15 downto 0);
+    signal mul : signed(25 downto 0);
  
   
 ----------------------------------------------------------------------------
@@ -27,8 +28,8 @@ begin
 	-- Pre-Regs
 	r1_sg 		<= signed(register1); 
 	--muxes
-	mux_r 		<= signed(data_in) when sel_mux(1) = '1'  else signed(mux_a);
-	mux_a 		<= std_logic_vector(r2_sg) when sel_mux(0) = '1' else std_logic_vector(r1_sg);
+	mux_r 		<= data_in(9) & data_in(9) & data_in(9) & data_in(9) & data_in(9) & data_in(9) & signed(data_in) when sel_mux(1) = '1'  else signed(mux_a);
+	mux_a 		<= std_logic_vector(r2_sg) when sel_mux(0) = '1' else r1_sg(9) & r1_sg(9) & r1_sg(9) & r1_sg(9) & r1_sg(9) & r1_sg(9) & std_logic_vector(r1_sg);
 
 
 	-- registo 1
@@ -36,7 +37,7 @@ begin
 	begin
 		if clk'event and clk = '1' then
 			if rst = '1' then
-				register1 <= x"00";
+				register1 <= "00" & x"00";
 			elsif load_hold(0) = '1' then 
 				register1 <= data_in;
 			end if;
@@ -48,7 +49,7 @@ begin
 	begin
 		if clk'event and clk = '1' then
 			if rst = '1' then
-				r2_sg <= x"00";
+				r2_sg <= (others => '0');
 			elsif load_hold(1) = '1' then 
 				r2_sg <= mux_r;
 			end if;
@@ -68,16 +69,16 @@ begin
             res_alu<= add_sub;
             
         when "010"=>
-            shift <= rotate_left( unsigned(r1_sg), 1);
+            shift <= (15 downto 10 => r1_sg(8)) & rotate_left( unsigned(r1_sg), 1);
             res_alu<=signed(shift);
         
         when  "011"=> 
-            logic<= std_logic_vector(r1_sg) and std_logic_vector(r2_sg);
+            logic<= ((15 downto 10 => '0') & std_logic_vector(r1_sg)) and std_logic_vector(r2_sg);
             res_alu<=signed(logic);
          
         when others => 
-            mul<=r1_sg*r2_sg;
-            res_alu<=mul;
+            mul <= r1_sg*r2_sg;
+            res_alu <= mul(15 downto 0);
             
         end case;
 end process;   
