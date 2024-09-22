4f80dda4c20e60789c7dad9faca418d360e5e16d
Barbichas <oeinsteineracareca@gmail.com>
2024-09-22 20:06:19 +0100

    concertei o MULS, grandes mudanças na FSM

--- LAB1/LAB1.srcs/sources_1/new/control_unit.vhd
+++ LAB1/LAB1.srcs/sources_1/new/control_unit.vhd
@@ -55,99 +55,48 @@ begin
 --turn currstate into next state --
     process(clk) begin
         
+        -- reset machine --
         if clk'event and clk = '1' then
             if (buttons(0) = '1') then 
             	rst_1 <= '1';
+            	sel_mux1 <= '1'; --default display is reg2--
                 curr_state <= FINAL;
+                
         
-            else  
-            curr_state <= next_state;
-			rst_1 <= '0';
+            else 
+                rst_1 <='0';
+                curr_state <= next_state;
             end if;
         end if;
     end process;
-    
---calculate next state based on inputs --
-    process(clk, curr_state) begin
-       -- next_state <= curr_state; --default is maintaining state --
-        if clk'event and clk = '1' then
-                case curr_state is
-                    when INITIAL =>
-                        if (buttons(4 downto 1)    ="0100") then
-                            if switch = '0' then
-                                next_state <= ADDS;
-                            else
-                                next_state <= SUBS;
-                            end if;
-                        elsif (buttons(4 downto 1) ="0010") then
-                            next_state <= MULS;
-                        elsif (buttons(4 downto 1) ="0001") then
-                            if switch = '0' then
-                                next_state <= LOGIC;
-                            else
-                                next_state <= SHIFT;
-                            end if;
-                        elsif (buttons(4 downto 1) = "1000") then
-                            if switch = '0' then
-                                next_state <= LOAD1;
-                            else
-                                next_state <= LOAD2;
-                            end if;
-                        end if;
-                    when ADDS    =>
-                        next_state <= FINAL;
-                    when SUBS    =>
-                        next_state <= FINAL;
-                    when MULS    =>
-                        next_state <= FINAL;
-                    when LOGIC   =>
-                        next_state <= FINAL;
-                    when SHIFT   =>
-                        next_state <= FINAL;
-                    when LOAD1   =>
-                        next_state <= FINAL;
-                    when LOAD2   =>
-                        next_state <= FINAL;
-                    when others  =>  -- FINAL --
-                        if ((buttons(4 downto 1) ="0000")) then
-                            next_state <= INITIAL;
-                        end if;
-                end case;
-        end if; 
-    end process;
-    
---calculate output based on current state ----
-    process(clk,curr_state) begin
-        if clk'event and clk = '1' then
-            case curr_state is
-                when INITIAL =>
-                    sel_mux2 <='0'; sel_mux1 <='0'; load_hold1 <='0'; load_hold2 <='0';
-                    sel_alu<="000";         
-                when ADDS    =>
-                    sel_mux2 <='0'; sel_mux1 <='1'; load_hold1 <='0'; load_hold2 <='1';
-                    sel_alu<="001";
-                when SUBS    =>
-                    sel_mux2 <='0'; sel_mux1 <='1'; load_hold1 <='0'; load_hold2 <='1';
-                    sel_alu<="000";
-                when MULS    =>
-                    sel_mux2 <='0'; sel_mux1 <='1'; load_hold1 <='0'; load_hold2 <='1';
-                    sel_alu<="1XX";
-                when LOGIC   =>
-                    sel_mux2 <='0'; sel_mux1 <='1'; load_hold1 <='0'; load_hold2 <='1';
-                    sel_alu<="011";
-                when SHIFT   =>
-                    sel_mux2 <='0'; sel_mux1 <='1'; load_hold1 <='0'; load_hold2 <='1';
-                    sel_alu<="010";
-                when LOAD1   =>
-                    sel_mux2 <='0'; sel_mux1 <='0'; load_hold1 <='1'; load_hold2 <='0';
-                    sel_alu<="XXX";
-                when LOAD2   =>
-                    sel_mux2 <='1'; sel_mux1 <='1'; load_hold1 <='0'; load_hold2 <='1';
-                    sel_alu<="XXX";
-                when others   =>  -- FINAL --
-                    sel_mux2 <='0'; load_hold1 <='0'; load_hold2 <='0';
-                    sel_alu<="000";
-            end case;
-        end if; 
-    end process;
+
+    next_state <= ADDS    when (curr_state = INITIAL) and (buttons(4 downto 1) = "0100") and (switch = '0') else
+                  SUBS    when (curr_state = INITIAL) and (buttons(4 downto 1) = "0100") and (switch = '1') else
+                  MULS    when (curr_state = INITIAL) and (buttons(4 downto 1) = "0010") else
+                  LOGIC   when (curr_state = INITIAL) and (buttons(4 downto 1) = "0001") and (switch = '0') else
+                  SHIFT   when (curr_state = INITIAL) and (buttons(4 downto 1) = "0001") and (switch = '1') else
+                  LOAD1   when (curr_state = INITIAL) and (buttons(4 downto 1) = "1000") and (switch = '0') else
+                  LOAD2   when (curr_state = INITIAL) and (buttons(4 downto 1) = "1000") and (switch = '1') else
+                  INITIAL when ((curr_state = FINAL) or(curr_state=INITIAL) )and (buttons(4 downto 1) = "0000") else
+                  FINAL;
+
+    sel_mux2   <= '1' when curr_state = LOAD2 else
+                  '0';
+
+    sel_mux1   <= '0' when curr_state = LOAD1 else
+                  '1';
+
+    load_hold1 <= '1' when curr_state = LOAD1 else 
+                  '0';
+
+    load_hold2 <= '0' when curr_state = LOAD1 or curr_state = INITIAL or curr_state = FINAL else
+                  '1';
+
+    sel_alu    <= "000" when curr_state = INITIAL or curr_state = SUBS or curr_state = FINAL else
+                  "001" when curr_state = ADDS else
+                  "100" when curr_state = MULS else
+                  "011" when curr_state = LOGIC else
+                  "010" when curr_state = SHIFT else
+                  "XXX";        
+                  
 end Behavioral;
