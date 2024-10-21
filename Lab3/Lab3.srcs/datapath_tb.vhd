4e57dd6a28c4e84d20037a1db5e12e0f4a9ed019
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-10-21 10:34:46 +0100

    Datapath tb

--- Lab3/Lab3.srcs/datapath_tb.vhd
+++ Lab3/Lab3.srcs/datapath_tb.vhd
@@ -36,31 +36,31 @@ entity datapath_tb is
 end datapath_tb;
 
 architecture Behavioral of datapath_tb is
+
 	component datapath is
-	  port (
+		port (
 		r11, r12, r21, r22, i11, i12, i21, i22 : in  std_logic_vector (11 downto 0);
-		sel_reg1, sel_reg2, sel_reg3, sel_reg4, sel_reg5 : in std_logic_vector (1 downto 0);
-		en_r1, en_r2, en_r3, en_r4,en_r5, en_r6, en_r7  : in  std_logic;
+		en_r1, en_r2, en_r3, en_r4,en_r5, en_r6, en_r7, en_r8, en_r9  : in  std_logic;
+		out_sumdetr, out_sumdeti, out_detr, out_deti : out std_logic_vector (31 downto 0);
 		clk, rst      : in  std_logic;
-		reg1 			 : out std_logic_vector (31 downto 0));
+		count_min , count_max : out std_logic_vector(7 downto 0));
 	end component;
 
 
     signal clk  :std_logic := '0';
     signal reset  :std_logic := '0';
-	signal sel_reg1, sel_reg2, sel_reg3, sel_reg4, sel_reg5 :   std_logic_vector (1 downto 0) := "11"; 
-	signal en_r1, en_r2, en_r3, en_r4,en_r5, en_r6, en_r7  :    std_logic := '1';
+	signal en_r1, en_r2, en_r3, en_r4,en_r5, en_r6, en_r7, en_r8, en_r9  :    std_logic := '1';
 
      -- Clock period definitions
     constant clk_period : time := 20 ns;
-	constant r11 std_logic_vector (11 downto 0):= x"000";
-	constant r22 std_logic_vector (11 downto 0):= x"000";
-	constant r12 std_logic_vector (11 downto 0):= x"001";
-	constant r21 std_logic_vector (11 downto 0):= x"001";
-	constant i11 std_logic_vector (11 downto 0):= x"001";
-	constant i22 std_logic_vector (11 downto 0):= x"001";
-	constant i21 std_logic_vector (11 downto 0):= x"000";
-	constant i12 std_logic_vector (11 downto 0):= x"000";
+	constant r11 : std_logic_vector (11 downto 0):= x"000";
+	constant r22 : std_logic_vector (11 downto 0):= x"000";
+	constant r12 : std_logic_vector (11 downto 0):= x"001";
+	constant r21 : std_logic_vector (11 downto 0):= x"001";
+	constant i11 : std_logic_vector (11 downto 0):= x"001";
+	constant i22 : std_logic_vector (11 downto 0):= x"001";
+	constant i21 : std_logic_vector (11 downto 0):= x"000";
+	constant i12 : std_logic_vector (11 downto 0):= x"000";
 
 begin
 	
@@ -76,18 +76,15 @@ begin
 	i12 => i12,
 	i21 => i21,
 	i22 => i22,
-	sel_reg1 =>sel_reg1,
-	sel_reg2 =>sel_reg2,
-	sel_reg3 =>sel_reg3,
-	sel_reg4 =>sel_reg4,
-	sel_reg5 =>sel_reg5,
-	en_r1 => en r1,
-	en_r2 => en r2,
-	en_r3 => en r3,
-	en_r4 => en r4,
-	en_r5 => en r5,
-	en_r6 => en r6,
-	en_r7 => en r7,
+	en_r1 => en_r1,
+	en_r2 => en_r2,
+	en_r3 => en_r3,
+	en_r4 => en_r4,
+	en_r5 => en_r5,
+	en_r6 => en_r6,
+	en_r7 => en_r7,
+	en_r8 => en_r8,
+	en_r9 => en_r9
 
 	);
 	
